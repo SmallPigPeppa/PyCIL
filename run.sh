4511e2fc5a309b0dc773317a04f12ab6045b694b
SmallPigPeppa <42854223+SmallPigPeppa@users.noreply.github.com>
2022-08-30 19:57:30 +0800

    initial

---
+++ run.sh
@@ -0,0 +1,13 @@
+conda activate torch
+python main.py --config=./exps/5samples_5steps/podnet.json
+python main.py --config=./exps/5samples_5steps/coil.json
+python main.py --config=./exps/5samples_5steps/der.json
+python main.py --config=./exps/5samples_5steps/lwf.json
+python main.py --config=./exps/5samples_5steps/icarl.json
+python main.py --config=./exps/5samples_5steps/ucir.json
+python main.py --config=./exps/5samples_10steps/podnet.json
+python main.py --config=./exps/5samples_10steps/coil.json
+python main.py --config=./exps/5samples_10steps/der.json
+python main.py --config=./exps/5samples_10steps/lwf.json
+python main.py --config=./exps/5samples_10steps/icarl.json
+python main.py --config=./exps/5samples_10steps/ucir.json
