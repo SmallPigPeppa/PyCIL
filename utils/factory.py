7328fc96b5762f08904b5e5b27bb6adf63b1be5a
SmallPigPeppa <42854223+SmallPigPeppa@users.noreply.github.com>
2022-09-01 14:44:54 +0800

    update master

--- utils/factory.py
+++ utils/factory.py
@@ -2,22 +2,24 @@ from models.coil import COIL
 from models.der import DER
 from models.ewc import EWC
 from models.finetune import Finetune
+from models.foster import FOSTER
 from models.gem import GEM
 from models.icarl import iCaRL
 from models.lwf import LwF
 from models.replay import Replay
 from models.bic import BiC
 from models.podnet import PODNet
+from models.rmm import RMM_FOSTER, RMM_iCaRL
 from models.wa import WA
 
 
 def get_model(model_name, args):
     name = model_name.lower()
-    if name == 'icarl':
+    if name == "icarl":
         return iCaRL(args)
-    elif name == 'bic':
+    elif name == "bic":
         return BiC(args)
-    elif name == 'podnet':
+    elif name == "podnet":
         return PODNet(args)
     elif name == "lwf":
         return LwF(args)
@@ -35,5 +37,11 @@ def get_model(model_name, args):
         return GEM(args)
     elif name == "coil":
         return COIL(args)
+    elif name == "foster":
+        return FOSTER(args)
+    elif name == "rmm-icarl":
+        return RMM_iCaRL(args)
+    elif name == "rmm-foster":
+        return RMM_FOSTER(args)
     else:
         assert 0
