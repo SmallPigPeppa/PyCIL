3d93e0cffd773544dab87e621aecf97ce83dc4c1
SmallPigPeppa <42854223+SmallPigPeppa@users.noreply.github.com>
2022-09-03 18:36:10 +0800

    update master

--- run_pretrain.sh
+++ run_pretrain.sh
@@ -95,9 +95,25 @@ python main.py --config=./exps/5samples_10steps_pretrain/coil.json
 
 
 
-/root/miniconda3/envs/torch/bin/python python main.py --config=./exps/5samples_5steps_imagenet100/lwf.json
-/root/miniconda3/envs/torch/bin/python python main.py --config=./exps/5samples_5steps_imagenet100/icarl.json
-/root/miniconda3/envs/torch/bin/python python main.py --config=./exps/5samples_5steps_imagenet100/coil.json
+/root/miniconda3/envs/torch/bin/python main.py --config=./exps/5samples_5steps_imagenet100/lwf.json
+/root/miniconda3/envs/torch/bin/python main.py --config=./exps/5samples_10steps_imagenet100/lwf.json
+/root/miniconda3/envs/torch/bin/python main.py --config=./exps/10samples_5steps_imagenet100/lwf.json
+/root/miniconda3/envs/torch/bin/python main.py --config=./exps/10samples_10steps_imagenet100/lwf.json
+
+
+
+
+
+/root/miniconda3/envs/torch/bin/python main.py --config=./exps/5samples_5steps_imagenet100/icarl.json
+/root/miniconda3/envs/torch/bin/python main.py --config=./exps/5samples_10steps_imagenet100/icarl.json
+/root/miniconda3/envs/torch/bin/python main.py --config=./exps/10samples_5steps_imagenet100/icarl.json
+/root/miniconda3/envs/torch/bin/python main.py --config=./exps/10samples_10steps_imagenet100/icarl.json
+
+
+/root/miniconda3/envs/torch/bin/python main.py --config=./exps/5samples_5steps_imagenet100/coil.json
+/root/miniconda3/envs/torch/bin/python main.py --config=./exps/5samples_10steps_imagenet100/coil.json
+/root/miniconda3/envs/torch/bin/python main.py --config=./exps/10samples_5steps_imagenet100/coil.json
+/root/miniconda3/envs/torch/bin/python main.py --config=./exps/10samples_10steps_imagenet100/coil.json
 
 
 
