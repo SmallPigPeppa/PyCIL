f01a0f1ae8b9a60673a3ee8dc2749014e84fada1
SmallPigPeppa <42854223+SmallPigPeppa@users.noreply.github.com>
2022-09-03 15:23:56 +0800

    update master

---
+++ run_pretrain.sh
@@ -0,0 +1,19 @@
+conda activate torch
+python main.py --config=./exps/5samples_5steps_pretrain/podnet.json
+python main.py --config=./exps/5samples_5steps_pretrain/coil.json
+python main.py --config=./exps/5samples_5steps_pretrain/der.json
+python main.py --config=./exps/5samples_5steps_pretrain/lwf.json
+python main.py --config=./exps/5samples_5steps_pretrain/icarl.json
+python main.py --config=./exps/5samples_5steps_pretrain/ucir.json
+
+
+python main.py --config=./exps/5samples_5steps_pretrain/podnet.json
+python main.py --config=./exps/5samples_5steps_pretrain/coil.json
+python main.py --config=./exps/5samples_5steps_pretrain/der.json
+python main.py --config=./exps/5samples_5steps_pretrain/lwf.json
+python main.py --config=./exps/5samples_5steps_pretrain/icarl.json
+python main.py --config=./exps/5samples_5steps_pretrain/ucir.json
+
+
+
+#CUDA_VISIBLE_DEVICES=4,5,6,7
