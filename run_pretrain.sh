54e884e55835b85bb3e59c55ac005d987b129db4
SmallPigPeppa <42854223+SmallPigPeppa@users.noreply.github.com>
2022-09-03 18:19:01 +0800

    update master

--- run_pretrain.sh
+++ run_pretrain.sh
@@ -70,20 +70,31 @@ python main.py --config=./exps/5samples_10steps_pretrain/coil.json
 
 
 
-/root/miniconda3/envs/torch/bin/python python main.py --config=./exps/5samples_5steps_imagenet100/podnet.json
-/root/miniconda3/envs/torch/bin/python python main.py --config=./exps/5samples_10steps_imagenet100/podnet.json
-/root/miniconda3/envs/torch/bin/python python main.py --config=./exps/10samples_5steps_imagenet100/podnet.json
-/root/miniconda3/envs/torch/bin/python python main.py --config=./exps/10samples_10steps_imagenet100/podnet.json
+/root/miniconda3/envs/torch/bin/python main.py --config=./exps/5samples_5steps_imagenet100/podnet.json
+/root/miniconda3/envs/torch/bin/python main.py --config=./exps/5samples_10steps_imagenet100/podnet.json
+/root/miniconda3/envs/torch/bin/python main.py --config=./exps/10samples_5steps_imagenet100/podnet.json
+/root/miniconda3/envs/torch/bin/python main.py --config=./exps/10samples_10steps_imagenet100/podnet.json
 
-/root/miniconda3/envs/torch/bin/python python main.py --config=./exps/5samples_5steps_imagenet100/ucir.json
-/root/miniconda3/envs/torch/bin/python python main.py --config=./exps/5samples_10steps_imagenet100/ucir.json
-/root/miniconda3/envs/torch/bin/python python main.py --config=./exps/10samples_5steps_imagenet100/ucir.json
-/root/miniconda3/envs/torch/bin/python python main.py --config=./exps/10samples_10steps_imagenet100/ucir.json
-
-
+/root/miniconda3/envs/torch/bin/python main.py --config=./exps/5samples_5steps_imagenet100/ucir.json
+/root/miniconda3/envs/torch/bin/python main.py --config=./exps/5samples_10steps_imagenet100/ucir.json
+/root/miniconda3/envs/torch/bin/python main.py --config=./exps/10samples_5steps_imagenet100/ucir.json
+/root/miniconda3/envs/torch/bin/python main.py --config=./exps/10samples_10steps_imagenet100/ucir.json
 
 
-/root/miniconda3/envs/torch/bin/python python main.py --config=./exps/5samples_5steps_imagenet100/der.json
+
+
+/root/miniconda3/envs/torch/bin/python main.py --config=./exps/5samples_5steps_imagenet100/der.json
+/root/miniconda3/envs/torch/bin/python main.py --config=./exps/5samples_10steps_imagenet100/der.json
+/root/miniconda3/envs/torch/bin/python main.py --config=./exps/10samples_5steps_imagenet100/der.json
+/root/miniconda3/envs/torch/bin/python main.py --config=./exps/10samples_10steps_imagenet100/der.json
+
+
+
+
+
+
+
+
 /root/miniconda3/envs/torch/bin/python python main.py --config=./exps/5samples_5steps_imagenet100/lwf.json
 /root/miniconda3/envs/torch/bin/python python main.py --config=./exps/5samples_5steps_imagenet100/icarl.json
 /root/miniconda3/envs/torch/bin/python python main.py --config=./exps/5samples_5steps_imagenet100/coil.json
