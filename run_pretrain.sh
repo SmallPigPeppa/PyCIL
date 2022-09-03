cc8b9e3c2ce21b7b83df684d16784f3f8d6e7091
SmallPigPeppa <42854223+SmallPigPeppa@users.noreply.github.com>
2022-09-03 15:24:32 +0800

    update master

--- run_pretrain.sh
+++ run_pretrain.sh
@@ -7,12 +7,12 @@ python main.py --config=./exps/5samples_5steps_pretrain/icarl.json
 python main.py --config=./exps/5samples_5steps_pretrain/ucir.json
 
 
-python main.py --config=./exps/5samples_5steps_pretrain/podnet.json
-python main.py --config=./exps/5samples_5steps_pretrain/coil.json
-python main.py --config=./exps/5samples_5steps_pretrain/der.json
-python main.py --config=./exps/5samples_5steps_pretrain/lwf.json
-python main.py --config=./exps/5samples_5steps_pretrain/icarl.json
-python main.py --config=./exps/5samples_5steps_pretrain/ucir.json
+python main.py --config=./exps/5samples_10steps_pretrain/podnet.json
+python main.py --config=./exps/5samples_10steps_pretrain/coil.json
+python main.py --config=./exps/5samples_10steps_pretrain/der.json
+python main.py --config=./exps/5samples_10steps_pretrain/lwf.json
+python main.py --config=./exps/5samples_10steps_pretrain/icarl.json
+python main.py --config=./exps/5samples_10steps_pretrain/ucir.json
 
 
 
