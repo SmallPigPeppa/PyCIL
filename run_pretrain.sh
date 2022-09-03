9361100c710e7a6743b33664c9fd8453d9aa2d51
SmallPigPeppa <42854223+SmallPigPeppa@users.noreply.github.com>
2022-09-03 16:34:35 +0800

    update master

--- run_pretrain.sh
+++ run_pretrain.sh
@@ -29,4 +29,23 @@ python main.py --config=./exps/5samples_10steps_pretrain/coil.json
 /root/miniconda3/envs/torch/bin/python python main.py --config=./exps/5samples_10steps_pretrain/icarl.json
 /root/miniconda3/envs/torch/bin/python python main.py --config=./exps/5samples_10steps_pretrain/coil.json
 
+
+
+
+/root/miniconda3/envs/torch/bin/python python main.py --config=./exps/10samples_5steps/podnet.json
+/root/miniconda3/envs/torch/bin/python python main.py --config=./exps/10samples_5steps/ucir.json
+/root/miniconda3/envs/torch/bin/python python main.py --config=./exps/10samples_5steps/der.json
+/root/miniconda3/envs/torch/bin/python python main.py --config=./exps/10samples_5steps/lwf.json
+/root/miniconda3/envs/torch/bin/python python main.py --config=./exps/10samples_5steps/icarl.json
+/root/miniconda3/envs/torch/bin/python python main.py --config=./exps/10samples_5steps/coil.json
+
+
+
+/root/miniconda3/envs/torch/bin/python python main.py --config=./exps/10samples_10steps/podnet.json
+/root/miniconda3/envs/torch/bin/python python main.py --config=./exps/10samples_10steps/ucir.json
+/root/miniconda3/envs/torch/bin/python python main.py --config=./exps/10samples_10steps/der.json
+/root/miniconda3/envs/torch/bin/python python main.py --config=./exps/10samples_10steps/lwf.json
+/root/miniconda3/envs/torch/bin/python python main.py --config=./exps/10samples_10steps/icarl.json
+/root/miniconda3/envs/torch/bin/python python main.py --config=./exps/10samples_10steps/coil.json
+
 #CUDA_VISIBLE_DEVICES=4,5,6,7
