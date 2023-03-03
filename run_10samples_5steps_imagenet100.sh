4f1d1cd0e66d10f3ad86cb311abd754b6e4c717e
SmallPigPeppa <42854223+SmallPigPeppa@users.noreply.github.com>
2023-03-04 00:56:50 +0800

    make coil num samples according json

---
+++ run_10samples_5steps_imagenet100.sh
@@ -0,0 +1,7 @@
+/root/miniconda3/envs/torch/bin/python main.py --config=./exps/10samples_5steps_pretrain_imagenet100/podnet.json
+/root/miniconda3/envs/torch/bin/python main.py --config=./exps/10samples_5steps_pretrain_imagenet100/ucir.json
+/root/miniconda3/envs/torch/bin/python main.py --config=./exps/10samples_5steps_pretrain_imagenet100/der.json
+/root/miniconda3/envs/torch/bin/python main.py --config=./exps/10samples_5steps_pretrain_imagenet100/lwf.json
+/root/miniconda3/envs/torch/bin/python main.py --config=./exps/10samples_5steps_pretrain_imagenet100/icarl.json
+/root/miniconda3/envs/torch/bin/python main.py --config=./exps/10samples_5steps_pretrain_imagenet100/coil.json
+
