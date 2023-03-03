a087ed30b12a20db651e5d30ba8a5a9930100dc8
SmallPigPeppa <42854223+SmallPigPeppa@users.noreply.github.com>
2023-03-03 23:43:57 +0800

    make coil num samples according json

---
+++ run_5samples_5steps.sh
@@ -0,0 +1,7 @@
+/root/miniconda3/envs/torch/bin/python main.py --config=./exps/5samples_5steps_pretrain/podnet.json
+/root/miniconda3/envs/torch/bin/python main.py --config=./exps/5samples_5steps_pretrain/ucir.json
+/root/miniconda3/envs/torch/bin/python main.py --config=./exps/5samples_5steps_pretrain/der.json
+/root/miniconda3/envs/torch/bin/python main.py --config=./exps/5samples_5steps_pretrain/lwf.json
+/root/miniconda3/envs/torch/bin/python main.py --config=./exps/5samples_5steps_pretrain/icarl.json
+/root/miniconda3/envs/torch/bin/python main.py --config=./exps/5samples_5steps_pretrain/coil.json
+
---
+++ run_all.sh
@@ -0,0 +1,111 @@
+
+
+
+/root/miniconda3/envs/torch/bin/python main.py --config=./exps/5samples_5steps_pretrain/ucir.json
+/root/miniconda3/envs/torch/bin/python python main.py --config=./exps/5samples_5steps_pretrain/coil.json
+/root/miniconda3/envs/torch/bin/python python main.py --config=./exps/5samples_5steps_pretrain/lwf.json
+/root/miniconda3/envs/torch/bin/python python main.py --config=./exps/5samples_5steps_pretrain/icarl.json
+
+
+
+/root/miniconda3/envs/torch/bin/python main.py --config=./exps/5samples_10steps_pretrain/podnet.json
+/root/miniconda3/envs/torch/bin/python main.py --config=./exps/5samples_10steps_pretrain/ucir.json
+/root/miniconda3/envs/torch/bin/python main.py --config=./exps/5samples_10steps_pretrain/der.json
+/root/miniconda3/envs/torch/bin/python main.py --config=./exps/5samples_10steps_pretrain/lwf.json
+/root/miniconda3/envs/torch/bin/python main.py --config=./exps/5samples_10steps_pretrain/icarl.json
+/root/miniconda3/envs/torch/bin/python main.py --config=./exps/5samples_10steps_pretrain/coil.json
+
+
+
+
+/root/miniconda3/envs/torch/bin/python python main.py --config=./exps/10samples_5steps/podnet.json
+/root/miniconda3/envs/torch/bin/python python main.py --config=./exps/10samples_5steps/ucir.json
+/root/miniconda3/envs/torch/bin/python python main.py --config=./exps/10samples_5steps/der.json
+/root/miniconda3/envs/torch/bin/python python main.py --config=./exps/10samples_5steps/lwf.json
+/root/miniconda3/envs/torch/bin/python python main.py --config=./exps/10samples_5steps/icarl.json
+/root/miniconda3/envs/torch/bin/python python main.py --config=./exps/10samples_5steps/coil.json
+
+
+
+/root/miniconda3/envs/torch/bin/python python main.py --config=./exps/10samples_10steps/podnet.json
+/root/miniconda3/envs/torch/bin/python python main.py --config=./exps/10samples_10steps/ucir.json
+/root/miniconda3/envs/torch/bin/python python main.py --config=./exps/10samples_10steps/der.json
+/root/miniconda3/envs/torch/bin/python python main.py --config=./exps/10samples_10steps/lwf.json
+/root/miniconda3/envs/torch/bin/python python main.py --config=./exps/10samples_10steps/icarl.json
+/root/miniconda3/envs/torch/bin/python python main.py --config=./exps/10samples_10steps/coil.json
+
+#CUDA_VISIBLE_DEVICES=4,5,6,7
+
+
+
+/root/miniconda3/envs/torch/bin/python python main.py --config=./exps/10samples_5steps/podnet.json
+/root/miniconda3/envs/torch/bin/python python main.py --config=./exps/10samples_5steps/ucir.json
+/root/miniconda3/envs/torch/bin/python python main.py --config=./exps/10samples_5steps/der.json
+/root/miniconda3/envs/torch/bin/python python main.py --config=./exps/10samples_5steps/lwf.json
+/root/miniconda3/envs/torch/bin/python python main.py --config=./exps/10samples_5steps/icarl.json
+/root/miniconda3/envs/torch/bin/python python main.py --config=./exps/10samples_5steps/coil.json
+
+
+
+/root/miniconda3/envs/torch/bin/python python main.py --config=./exps/10samples_10steps/podnet.json
+/root/miniconda3/envs/torch/bin/python python main.py --config=./exps/10samples_10steps/ucir.json
+/root/miniconda3/envs/torch/bin/python python main.py --config=./exps/10samples_10steps/der.json
+/root/miniconda3/envs/torch/bin/python python main.py --config=./exps/10samples_10steps/lwf.json
+/root/miniconda3/envs/torch/bin/python python main.py --config=./exps/10samples_10steps/icarl.json
+/root/miniconda3/envs/torch/bin/python python main.py --config=./exps/10samples_10steps/coil.json
+
+
+
+/root/miniconda3/envs/torch/bin/python main.py --config=./exps/5samples_5steps_imagenet100/podnet.json
+/root/miniconda3/envs/torch/bin/python main.py --config=./exps/5samples_10steps_imagenet100/podnet.json
+/root/miniconda3/envs/torch/bin/python main.py --config=./exps/10samples_5steps_imagenet100/podnet.json
+/root/miniconda3/envs/torch/bin/python main.py --config=./exps/10samples_10steps_imagenet100/podnet.json
+
+/root/miniconda3/envs/torch/bin/python main.py --config=./exps/5samples_5steps_imagenet100/ucir.json
+/root/miniconda3/envs/torch/bin/python main.py --config=./exps/5samples_10steps_imagenet100/ucir.json
+/root/miniconda3/envs/torch/bin/python main.py --config=./exps/10samples_5steps_imagenet100/ucir.json
+/root/miniconda3/envs/torch/bin/python main.py --config=./exps/10samples_10steps_imagenet100/ucir.json
+
+
+
+
+/root/miniconda3/envs/torch/bin/python main.py --config=./exps/5samples_5steps_imagenet100/der.json
+/root/miniconda3/envs/torch/bin/python main.py --config=./exps/5samples_10steps_imagenet100/der.json
+/root/miniconda3/envs/torch/bin/python main.py --config=./exps/10samples_5steps_imagenet100/der.json
+/root/miniconda3/envs/torch/bin/python main.py --config=./exps/10samples_10steps_imagenet100/der.json
+
+
+
+
+
+
+
+
+/root/miniconda3/envs/torch/bin/python main.py --config=./exps/5samples_5steps_imagenet100/lwf.json
+/root/miniconda3/envs/torch/bin/python main.py --config=./exps/5samples_10steps_imagenet100/lwf.json
+/root/miniconda3/envs/torch/bin/python main.py --config=./exps/10samples_5steps_imagenet100/lwf.json
+/root/miniconda3/envs/torch/bin/python main.py --config=./exps/10samples_10steps_imagenet100/lwf.json
+
+
+
+
+
+/root/miniconda3/envs/torch/bin/python main.py --config=./exps/5samples_5steps_imagenet100/icarl.json
+/root/miniconda3/envs/torch/bin/python main.py --config=./exps/5samples_10steps_imagenet100/icarl.json
+/root/miniconda3/envs/torch/bin/python main.py --config=./exps/10samples_5steps_imagenet100/icarl.json
+/root/miniconda3/envs/torch/bin/python main.py --config=./exps/10samples_10steps_imagenet100/icarl.json
+
+
+/root/miniconda3/envs/torch/bin/python main.py --config=./exps/5samples_5steps_imagenet100/coil.json
+/root/miniconda3/envs/torch/bin/python main.py --config=./exps/5samples_10steps_imagenet100/coil.json
+/root/miniconda3/envs/torch/bin/python main.py --config=./exps/10samples_5steps_imagenet100/coil.json
+/root/miniconda3/envs/torch/bin/python main.py --config=./exps/10samples_10steps_imagenet100/coil.json
+
+
+
+/root/miniconda3/envs/torch/bin/python python main.py --config=./exps/10samples_10steps/podnet.json
+/root/miniconda3/envs/torch/bin/python python main.py --config=./exps/10samples_10steps/ucir.json
+/root/miniconda3/envs/torch/bin/python python main.py --config=./exps/10samples_10steps/der.json
+/root/miniconda3/envs/torch/bin/python python main.py --config=./exps/10samples_10steps/lwf.json
+/root/miniconda3/envs/torch/bin/python python main.py --config=./exps/10samples_10steps/icarl.json
+/root/miniconda3/envs/torch/bin/python python main.py --config=./exps/10samples_10steps/coil.json
