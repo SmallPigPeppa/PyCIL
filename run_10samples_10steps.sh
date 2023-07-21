e3b22a91ea05edead08dc66dcf0242a6cdf06330
lwz <905328134@qq.com>
2023-07-21 22:35:25 +0800

    fix coil memory size

--- run_10samples_10steps.sh
+++ run_10samples_10steps.sh
@@ -1,7 +1,7 @@
-/root/miniconda3/envs/torch/bin/python main.py --config=./exps/10samples_10steps_pretrain/podnet.json
-/root/miniconda3/envs/torch/bin/python main.py --config=./exps/10samples_10steps_pretrain/ucir.json
-/root/miniconda3/envs/torch/bin/python main.py --config=./exps/10samples_10steps_pretrain/der.json
-/root/miniconda3/envs/torch/bin/python main.py --config=./exps/10samples_10steps_pretrain/lwf.json
-/root/miniconda3/envs/torch/bin/python main.py --config=./exps/10samples_10steps_pretrain/icarl.json
+/root/miniconda3/envs/torch/bin/python main.py --config=./exps/10samples_10steps/podnet.json
+/root/miniconda3/envs/torch/bin/python main.py --config=./exps/10samples_10steps/ucir.json
+/root/miniconda3/envs/torch/bin/python main.py --config=./exps/10samples_10steps/der.json
+/root/miniconda3/envs/torch/bin/python main.py --config=./exps/10samples_10steps/lwf.json
+/root/miniconda3/envs/torch/bin/python main.py --config=./exps/10samples_10steps/icarl.json
 #/root/miniconda3/envs/torch/bin/python main.py --config=./exps/10samples_10steps_pretrain/coil.json
 
