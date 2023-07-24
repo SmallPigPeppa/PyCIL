1e72c57655a1eeab6c4a74c03ed1131f45b2637c
lwz <905328134@qq.com>
2023-07-24 15:08:54 +0800

    fix coil memory size

--- run_10samples_10steps.sh
+++ run_10samples_10steps.sh
@@ -1,8 +1,8 @@
-/root/miniconda3/envs/torch/bin/python main.py --config=./exps/10samples_10steps/podnet.json
-/root/miniconda3/envs/torch/bin/python main.py --config=./exps/10samples_10steps/ucir.json
+#/root/miniconda3/envs/torch/bin/python main.py --config=./exps/10samples_10steps/podnet.json
+#/root/miniconda3/envs/torch/bin/python main.py --config=./exps/10samples_10steps/ucir.json
 /root/miniconda3/envs/torch/bin/python main.py --config=./exps/10samples_10steps/der.json
-/root/miniconda3/envs/torch/bin/python main.py --config=./exps/10samples_10steps/lwf.json
-/root/miniconda3/envs/torch/bin/python main.py --config=./exps/10samples_10steps/icarl.json
+#/root/miniconda3/envs/torch/bin/python main.py --config=./exps/10samples_10steps/lwf.json
+#/root/miniconda3/envs/torch/bin/python main.py --config=./exps/10samples_10steps/icarl.json
 #/root/miniconda3/envs/torch/bin/python main.py --config=./exps/10samples_10steps_pretrain/coil.json
 
 
--- run_10samples_5steps.sh
+++ run_10samples_5steps.sh
@@ -1,7 +1,7 @@
-/root/miniconda3/envs/torch/bin/python main.py --config=./exps/10samples_5steps/podnet.json
-/root/miniconda3/envs/torch/bin/python main.py --config=./exps/10samples_5steps/ucir.json
+#/root/miniconda3/envs/torch/bin/python main.py --config=./exps/10samples_5steps/podnet.json
+#/root/miniconda3/envs/torch/bin/python main.py --config=./exps/10samples_5steps/ucir.json
 /root/miniconda3/envs/torch/bin/python main.py --config=./exps/10samples_5steps/der.json
-/root/miniconda3/envs/torch/bin/python main.py --config=./exps/10samples_5steps/lwf.json
-/root/miniconda3/envs/torch/bin/python main.py --config=./exps/10samples_5steps/icarl.json
+#/root/miniconda3/envs/torch/bin/python main.py --config=./exps/10samples_5steps/lwf.json
+#/root/miniconda3/envs/torch/bin/python main.py --config=./exps/10samples_5steps/icarl.json
 #/root/miniconda3/envs/torch/bin/python main.py --config=./exps/10samples_5steps_pretrain/coil.json
 
